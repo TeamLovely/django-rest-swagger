573c5d26d190264aef912ffb7635efc3440b7d12
Diego Lorden <djlorden@gmail.com>
2015-01-09 11:55:08 -0800

    Add unversioned URLs for backwards compatibility

--- rest_framework_swagger/urls.py
+++ rest_framework_swagger/urls.py
@@ -6,6 +6,8 @@ from rest_framework_swagger.views import SwaggerResourcesView, SwaggerApiView, S
 urlpatterns = patterns(
     '',
     url(r'^$', SwaggerUIView.as_view(), name="django.swagger.base.view"),
+    url(r'^api-docs/$', SwaggerResourcesView.as_view(), name="django.swagger.resources.view"),
+    url(r'^api-docs/(?P<path>.*)/?$', SwaggerApiView.as_view(), name='django.swagger.api.view'),
     url(r'^v(?P<api_version>[0-9.]+)/$', SwaggerUIView.as_view(), name="django.swagger.base.view"),
     url(r'^v(?P<api_version>[0-9.]+)/api-docs/$', SwaggerResourcesView.as_view(), name="django.swagger.resources.view"),
     url(r'^v(?P<api_version>[0-9.]+)/api-docs/(?P<path>.*)/?$', SwaggerApiView.as_view(), name='django.swagger.api.view'),
